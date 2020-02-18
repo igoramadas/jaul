7799054ed3d4a85ca27f410e2af2b20197eabb3a
Igor Ramadas <igor@devv.com>
2020-02-18 21:30:26 +0100

    Removed lib folder.

--- lib/data.d.ts
+++
@@ -1,9 +0,0 @@
-declare class DataUtils {
-    static removeFromString(value: string, charsToRemove: any[] | string): string;
-    static replaceTags: (text: string, obj: any) => string;
-    static maskString(value: string, maskChar?: string, leaveLast?: number): string;
-    static minifyJson(source: string, asString?: boolean): any;
-    static stripHtml(html: string, tagReplace?: string): string;
-    static uuid(): string;
-}
-export = DataUtils;
--- lib/network.d.ts
+++
@@ -1,8 +0,0 @@
-declare class NetworkUtils {
-    static getIP(family: string): string[];
-    static getSingleIPv4(): string;
-    static getSingleIPv6(): string;
-    static getClientIP(reqOrSocket: any): string;
-    static ipInRange(ip: string, range: string[] | string): boolean;
-}
-export = NetworkUtils;
